927488b537c03262625e0dacc8e4cf57c5919076
Lucas Hebmuller <lucas.hebmuller@gmail.com>
2025-12-12 08:05:40 -0500

    refactor name

--- src/main/java/com/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/expensetracker/ExpenseTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.expensetracker.expense_tracker;
+package com.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/expensetracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/com/expensetracker/ExpenseTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.expensetracker.expense_tracker;
+package com.expensetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

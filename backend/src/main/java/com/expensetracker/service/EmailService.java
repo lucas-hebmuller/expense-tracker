6aaffc979723ecd223e099e7735f7df9b80c0a9f
Lucas Hebmuller <lucas.hebmuller@gmail.com>
2026-09-14 07:32:16 -0400

    email sending works

---
+++ backend/src/main/java/com/expensetracker/service/EmailService.java
@@ -0,0 +1,26 @@
+package com.expensetracker.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${app.mail.from}")
+    private String fromAddress;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromAddress);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
